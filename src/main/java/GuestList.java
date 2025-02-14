a7d84c79f67df1e878db5bce6228d2e6091b508c
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-14 16:26:10 +0100

    bonus test 3

--- src/main/java/GuestList.java
+++ src/main/java/GuestList.java
@@ -1,9 +1,27 @@
 import lombok.Data;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 @Data
 public class GuestList
 {
-    List<String> guests = new ArrayList<>();
+    List<String> guests;
+
+    public void setGuests(List<String> guests)
+    {
+        this.guests = guests;
+
+        Path filePath = Path.of("guests.txt");
+        try
+        {
+            Files.write(filePath, guests);
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException("Failed to write to file", e);
+        }
+
+    }
 }
--- src/test/java/GuestListTest.java
+++ src/test/java/GuestListTest.java
@@ -1,6 +1,9 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,6 +12,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GuestListTest
 {
+    private final Path filePath = Path.of("guests.txt");
+
+    @AfterEach
+    public void cleanUp() throws IOException
+    {
+        Files.deleteIfExists(filePath);
+    }
 
     @Test
     public void shouldBeEmptyInitially()
@@ -27,12 +37,27 @@ public class GuestListTest
     {
         GuestList guestList = new GuestList();
 
-        List<String> inputGuests = Arrays.asList("Karl", "Ute");
+        List<String> inputGuests = List.of("Karl", "Ute");
         guestList.setGuests(inputGuests);
 
         List<String> guests = guestList.getGuests();
 
         assertEquals(inputGuests, guests, "The guest list returned is not the same as the one set.");
+    }
+
+    @Test
+    public void shouldWriteToFileSystem() throws IOException
+    {
+        GuestList guestList = new GuestList();
+
+        List<String> guests = List.of("Theodor", "Anette");
+        guestList.setGuests(guests);
+
+        assertTrue(Files.exists(filePath), "The file guests.txt should be created.");
+
+        List<String> fileContent = Files.readAllLines(filePath);
+
+        assertEquals(guests, fileContent, "The content of guests.txt does not match the list of guests.");
     }
 
 }
